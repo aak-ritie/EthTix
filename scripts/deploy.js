f3f029c89b1aafa3dd8c941b4972a3e56c7c1885
Aakriti Bhusal <117297031+aak-ritie@users.noreply.github.com>
2025-01-14 16:05:24 +0545

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,37 +1,11 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
 const fs = require("fs");
 
 async function main() {
-  // const AccessControl = await ethers.getContractFactory("AccessControl");
-  // const accessControl = await AccessControl.deploy();
-  // await accessControl.deployed();
-
-  // const User = await ethers.getContractFactory("User");
-  // const user = await User.deploy();
-  // await user.deployed();
-
-  // const EventOrganizer = await hre.ethers.getContractFactory("EventOrganizer");
-  // const eventOrganizer = await EventOrganizer.deploy();
-  // await eventOrganizer.deployed();
-
   const Tickets = await hre.ethers.getContractFactory("Tickets");
   const tickets = await Tickets.deploy();
   await tickets.deployed();
-
-  //console.log("Tickets deployed to:", tickets.address);
-
-  // Write contract addresses to a JSON file
   const addresses = {
-    // user: user.target,
-    //accessControl: accessControl.address,
-    //user: user.address,
-    //eventOrganizer: eventOrganizer.address,
     tickets: tickets.address,
   };
 
@@ -40,8 +14,6 @@ async function main() {
   console.log("Contract addresses written to contractAddresses.json");
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
